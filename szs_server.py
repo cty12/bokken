d3d8bee1e855fb9ea96da53b4c3b56f78f222c41
Tianyu Chen <chentianyu@outlook.com>
2016-07-18 18:10:11 +0800

    redesigned the connections collection

--- szs_server.py
+++ szs_server.py
@@ -4,13 +4,34 @@ from Mastermind import *
 
 import threading
 
+class Connections:
+    def __init__(self):
+        self._all_conn = dict()
+        self._conn_serial = 0
+
+    def insert(self, conn):
+        self._all_conn[self._conn_serial] = conn
+        self._conn_serial += 1
+
+    def remove(self, conn):
+        for key, value in self._all_conn.iteritems():
+            if value == conn:
+                self._all_conn.pop(key)
+                break
+
+    def get_conn_objs(self):
+        return self._all_conn.values()
+
+    def get_conn_cnt(self):
+        return len(self._all_conn.keys())
+
 class SzsServer(MastermindServerTCP):
-    # list of client connection objects
-    connections = []
 
     def __init__(self):
         # server refresh; conn refresh; timeout
         MastermindServerTCP.__init__(self, 0.5,0.5,10.0)
+        # define the collection of all client connections
+        self._connections = Connections()
 
     def callback_connect(self):
         return super(SzsServer, self).callback_connect()
@@ -21,10 +42,15 @@ class SzsServer(MastermindServerTCP):
     def callback_connect_client(self, connection_object):
         # for debug
         print 'incoming client connection'
-        SzsServer.connections.append(connection_object)
+        self._connections.insert(connection_object)
+        print 'number of connections: ', self._connections.get_conn_cnt()
         return super(SzsServer, self).callback_connect_client(connection_object)
 
     def callback_disconnect_client(self, connection_object):
+        # for debug
+        print 'client disconnected'
+        self._connections.remove(connection_object)
+        print 'number of connections: ', self._connections.get_conn_cnt()
         return super(SzsServer, self).callback_disconnect_client(connection_object)
 
     def callback_client_receive(self, connection_object):
@@ -37,7 +63,7 @@ class SzsServer(MastermindServerTCP):
         elif data[0] == 'update':
             # update chessboard
             print 'col: ', data[1]['col'], 'row: ', data[1]['row'], 'icon: ', data[1]['icon']
-            for conn in SzsServer.connections:
+            for conn in self._connections.get_conn_objs():
                 self.callback_client_send(conn, data[1])
 
     def callback_client_send(self, connection_object, data, compression=None):
